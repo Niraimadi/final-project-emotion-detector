69cea8f4d2ad7c67478ca61415e829a20ead94e4
Niraimadi S <niraimadikpm@gmail.com>
2025-07-03 10:31:11 -0400

    Updated code for formatted response

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -8,20 +9,26 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url=url, headers=headers, json=myobj)
     formatted_response = json.loads(response.text)
-    
+
     anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
     disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
     fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
     joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
     sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
-    
-    emotion_dict = {"anger": anger_score, "disgust": disgust_score, "fear": fear_score, "joy": joy_score, "sadness": sadness_score}
-    emotion_score = 0
-    dominant_emotion = None
-    for key,val in emotion_dict.items():
-        if val >= emotion_score:
-            dominant_emotion = key
-    emotion_dict.update("dominant_emotion", dominant_emotion)
 
+    emotion_dict = {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+    }
+    emotion_score = float("-inf")
+    dominant_emotion = None
+    for key, val in emotion_dict.items():
+        if val > emotion_score:
+            dominant_emotion = key
+            emotion_score = val
+
+    emotion_dict["dominant_emotion"] = dominant_emotion
     return emotion_dict
- 
