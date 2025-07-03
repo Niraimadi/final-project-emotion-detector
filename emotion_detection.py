c9e1a647bcceca62015e22d8cea2627307ad967f
Niraimadi S <niraimadikpm@gmail.com>
2025-07-03 10:05:32 -0400

    Formatted result

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -7,4 +7,21 @@ def emotion_detector(text_to_analyze):
     myobj = {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(url=url, headers=headers, json=myobj)
-    return response.text
+    formatted_response = json.loads(response.text)
+    
+    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+    
+    emotion_dict = {"anger": anger_score, "disgust": disgust_score, "fear": fear_score, "joy": joy_score, "sadness": sadness_score}
+    emotion_score = 0
+    dominant_emotion = None
+    for key,val in emotion_dict.items():
+        if val >= emotion_score:
+            dominant_emotion = key
+    emotion_dict.update("dominant_emotion", dominant_emotion)
+
+    return emotion_dict
+ 
